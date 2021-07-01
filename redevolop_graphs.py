6e5e1520412ec240b86edb8d143a67fa3d47ddfc
deepkarman <ksselhi@gmail.com>
2021-07-02 00:17:06 +0530

    bringing upto date

--- redevolop_graphs.py
+++ redevolop_graphs.py
@@ -11,15 +11,22 @@ nums = []
 count = 0
 
 idx_slide = 0
-
+data_obj = None
+temp_obj = None
 for folder in list_folders:
 	files = sorted(glob.glob(folder+'/*'))
 	for file in files:
+		
 		with open(file, "rb") as curr_file:
-			data_obj = pickle.load(curr_file)
-		data_obj.append(idx_slide)
-		with open(file,"wb") as file_pickle:
-			pickle.dump(data_obj, file_pickle)
+			try:
+				data_obj = pickle.load(curr_file)
+				temp_obj = data_obj[2]
+			except:
+				pdb.set_trace()
+		if type(temp_obj) == int:
+			data_obj[2] = [temp_obj, idx_slide]
+			with open(file,"wb") as file_pickle:
+				pickle.dump(data_obj, file_pickle)
 	idx_slide += 1
 
 
